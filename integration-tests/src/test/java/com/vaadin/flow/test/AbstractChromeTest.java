d03cc04ffb5fcdbf9987e780c14ee80f5523a262
Mikael Grankvist <mikael.grankvist@vaadin.com>
2021-06-21 08:26:16 +0300

    Add desired capability for chrome

--- integration-tests/src/test/java/com/vaadin/flow/test/AbstractChromeTest.java
+++ integration-tests/src/test/java/com/vaadin/flow/test/AbstractChromeTest.java
@@ -1,21 +1,21 @@
 package com.vaadin.flow.test;
 
-import java.lang.management.ManagementFactory;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.function.Consumer;
 
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.experimental.categories.Category;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import com.vaadin.flow.testcategory.ChromeTests;
 import com.vaadin.flow.testutil.ChromeBrowserTest;
 import com.vaadin.testbench.ScreenshotOnFailureRule;
+import com.vaadin.testbench.annotations.BrowserConfiguration;
+import com.vaadin.testbench.parallel.Browser;
+import com.vaadin.testbench.parallel.BrowserUtil;
 
 /**
  * Simplified chrome test that doesn't handle view/IT class paths.
@@ -40,8 +40,15 @@ public abstract class AbstractChromeTest extends ChromeBrowserTest {
 
     @BeforeAll
     public void setup() throws Exception {
+        setDesiredCapabilities(Browser.CHROME.getDesiredCapabilities());
         super.setup();
     }
+
+    @BrowserConfiguration
+    public List<DesiredCapabilities> getBrowsersToTest() {
+        return Arrays.asList(BrowserUtil.chrome());
+    }
+
     @Override
     protected String getTestPath() {
         return "/";
