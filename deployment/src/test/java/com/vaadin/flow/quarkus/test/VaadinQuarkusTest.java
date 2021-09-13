10b244aedfc4af9b2897e21c49c851b72e2afa1b
Mikael Grankvist <mikael.grankvist@vaadin.com>
2021-09-13 13:54:00 +0300

    chore: add formatter and format code base
    Part of #15

--- deployment/src/test/java/com/vaadin/flow/quarkus/test/VaadinQuarkusTest.java
+++ deployment/src/test/java/com/vaadin/flow/quarkus/test/VaadinQuarkusTest.java
@@ -13,11 +13,14 @@ public class VaadinQuarkusTest {
     // Start unit test with your extension loaded
     @RegisterExtension
     static final QuarkusUnitTest unitTest = new QuarkusUnitTest()
-        .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class));
+            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class));
 
     @Test
     public void writeYourOwnUnitTest() {
-        // Write your unit tests here - see the testing extension guide https://quarkus.io/guides/writing-extensions#testing-extensions for more information
-        Assertions.assertTrue(true, "Add some assertions to " + getClass().getName());
+        // Write your unit tests here - see the testing extension guide
+        // https://quarkus.io/guides/writing-extensions#testing-extensions for
+        // more information
+        Assertions.assertTrue(true,
+                "Add some assertions to " + getClass().getName());
     }
 }
