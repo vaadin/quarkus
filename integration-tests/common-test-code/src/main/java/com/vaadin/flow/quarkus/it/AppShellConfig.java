ed234a44e7b2c2a21592630ce360a37fa45ebfad
Mikhail Shabarov <mikhail@vaadin.com>
2021-10-11 17:10:17 +0300

    test: Swap the Push to long polling
    After debug window improvements (https://github.com/vaadin/flow/pull/11980/files), the gizmo is shown even if the live reload is disabled.
    Thus, it tries to use web sockets still, which causes issues in tests.

--- integration-tests/common-test-code/src/main/java/com/vaadin/flow/quarkus/it/AppShellConfig.java
+++ integration-tests/common-test-code/src/main/java/com/vaadin/flow/quarkus/it/AppShellConfig.java
@@ -1,8 +1,12 @@
 package com.vaadin.flow.quarkus.it;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.Push;
+import com.vaadin.flow.shared.ui.Transport;
 import com.vaadin.flow.theme.Theme;
 
 @Theme("reusable-theme")
+// TODO: Enable websockets when they are in use in Quarkus
+@Push(transport = Transport.LONG_POLLING)
 public class AppShellConfig implements AppShellConfigurator {
 }
