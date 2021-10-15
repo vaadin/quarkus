52421939f117561646163dc748f241d6f38b3318
Mikhail Shabarov <61410877+mshabarov@users.noreply.github.com>
2021-10-15 18:20:07 +0300

    fix: Disable dev mode gizmo to not use web-sockets (#43)
    * Revert "test: Swap the Push to long polling (#41)"
    
    This reverts commit 578a5290
    
    * fix: Disable dev mode gizmo to not use web-sockets
    
    * fix: Disable dev mode gizmo in the servlet init params

--- integration-tests/common-test-code/src/main/java/com/vaadin/flow/quarkus/it/TestServlet.java
+++ integration-tests/common-test-code/src/main/java/com/vaadin/flow/quarkus/it/TestServlet.java
@@ -6,7 +6,10 @@ import javax.servlet.annotation.WebServlet;
 import com.vaadin.quarkus.QuarkusVaadinServlet;
 
 @WebServlet(urlPatterns = "/*", name = "TestServlet", asyncSupported = true, initParams = {
-        @WebInitParam(name = "org.atmosphere.websocket.suppressJSR356", value = "true") })
+        @WebInitParam(name = "org.atmosphere.websocket.suppressJSR356", value = "true"),
+        // TODO: Enable dev mode gizmo when websockets are in use
+        @WebInitParam(name = "devmode.gizmo.enabled", value = "false")})
 public class TestServlet extends QuarkusVaadinServlet {
-    // This servlet is here to give the suppressJSR356 parameter
+    // This servlet is here to give the suppressJSR356 parameter and disable
+    // dev mode gizmo which triggers unsupported web-sockets connection
 }
