0b210130056ad2fbb3a5f80b79cce88689646d78
Marco Collovati <mcollovati@gmail.com>
2022-02-01 09:24:06 +0100

    Applied review suggestions

--- integration-tests/test-addons/addon-with-jandex/src/main/java/org/vaadin/jandex/HelloWorldJandex.java
+++ integration-tests/test-addons/addon-with-jandex/src/main/java/org/vaadin/jandex/HelloWorldJandex.java
@@ -1,14 +1,14 @@
 package org.vaadin.jandex;
 
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.dependency.NpmPackage;
-import com.vaadin.flow.component.littemplate.LitTemplate;
 
 @Tag("hello-world-jandex")
 @NpmPackage(value = "@axa-ch/input-text", version = "4.3.11")
 @JsModule("./src/hello-world-jandex.ts")
-public class HelloWorldJandex extends LitTemplate {
+public class HelloWorldJandex extends Component {
 
     /**
      * Creates the hello world template.
--- integration-tests/test-addons/addon-without-jandex/src/main/java/org/vaadin/nojandex/HelloWorld.java
+++ integration-tests/test-addons/addon-without-jandex/src/main/java/org/vaadin/nojandex/HelloWorld.java
@@ -1,14 +1,14 @@
 package org.vaadin.nojandex;
 
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.dependency.NpmPackage;
-import com.vaadin.flow.component.littemplate.LitTemplate;
 
 @Tag("hello-world")
 @NpmPackage(value = "@axa-ch/input-text", version = "4.3.11")
 @JsModule("./src/hello-world.ts")
-public class HelloWorld extends LitTemplate {
+public class HelloWorld extends Component {
 
     /**
      * Creates the hello world template.
