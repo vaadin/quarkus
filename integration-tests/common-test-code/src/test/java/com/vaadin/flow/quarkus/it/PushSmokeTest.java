da620638fcac4bb1c5e79e732077d0d41f32afde
Marco Collovati <mcollovati@gmail.com>
2022-02-02 15:41:22 +0100

    chore: Add smoke test for PUSH (#42)
    Adds integration test to ensure PUSH works.
    At the moment only long polling is supported.
    
    Part of #4
    Closes #42

---
+++ integration-tests/common-test-code/src/test/java/com/vaadin/flow/quarkus/it/PushSmokeTest.java
@@ -0,0 +1,51 @@
+package com.vaadin.flow.quarkus.it;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import com.vaadin.flow.test.AbstractChromeTest;
+import com.vaadin.testbench.TestBenchElement;
+
+@QuarkusIntegrationTest
+public class PushSmokeTest extends AbstractChromeTest {
+
+    protected String getTestPath() {
+        return "/push";
+    }
+
+    @Test
+    public void pushUpdatesEmbeddedWebComponent() {
+        open();
+        waitForElementPresent(By.id("push-update"));
+
+        int expectedUpdates = 50;
+        int initialUpdateCount = getUpdateCount();
+        Assertions.assertTrue(initialUpdateCount < expectedUpdates,
+                "The initial update count should be less than maximum 50, but it has value "
+                        + initialUpdateCount);
+
+        waitUntil(driver -> getUpdateCount() > initialUpdateCount, 10);
+
+        int nextUpdateCount = getUpdateCount();
+
+        Assertions.assertTrue(nextUpdateCount < expectedUpdates,
+                "The next interim update count should be less than maximum 50, but it has value "
+                        + nextUpdateCount);
+
+        waitUntil(driver -> getUpdateCount() == expectedUpdates, 5);
+
+        int updateCount = getUpdateCount();
+        Assertions.assertEquals(expectedUpdates, updateCount,
+                "The update count should have reached the maximin 50, but it has value "
+                        + updateCount);
+    }
+
+    private int getUpdateCount() {
+        TestBenchElement div = $(TestBenchElement.class).id("push-update");
+        String count = div.getText();
+        return Integer.parseInt(count);
+    }
+
+}
