ebe1a30fe85a994b74ac8ccb3bced1ba00090e5f
Marco Collovati <marco@vaadin.com>
2022-03-01 11:23:51 +0100

    feat: Make Vaadin PUSH functionality work
    Added a build step to initialize a customized Vert.x handler that attaches
    HTTP session from upgrade request to the WebSocket exhange so that Vaadin
    PushHandler can work.

--- integration-tests/common-test-code/src/main/java/com/vaadin/flow/quarkus/it/AppShellConfig.java
+++ integration-tests/common-test-code/src/main/java/com/vaadin/flow/quarkus/it/AppShellConfig.java
@@ -2,10 +2,9 @@ package com.vaadin.flow.quarkus.it;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
-import com.vaadin.flow.shared.ui.Transport;
 import com.vaadin.flow.theme.Theme;
 
 @Theme("reusable-theme")
-@Push(transport = Transport.LONG_POLLING) // Websocket not supported currently
+@Push
 public class AppShellConfig implements AppShellConfigurator {
 }
--- integration-tests/common-test-code/src/main/java/com/vaadin/flow/quarkus/it/TestServlet.java
+++ integration-tests/common-test-code/src/main/java/com/vaadin/flow/quarkus/it/TestServlet.java
@@ -6,9 +6,8 @@ import javax.servlet.annotation.WebServlet;
 import com.vaadin.quarkus.QuarkusVaadinServlet;
 
 @WebServlet(urlPatterns = "/*", name = "TestServlet", asyncSupported = true, initParams = {
-        @WebInitParam(name = "org.atmosphere.websocket.suppressJSR356", value = "true"),
-        // TODO: Enable dev mode gizmo when websockets are in use
-        @WebInitParam(name = "devmode.gizmo.enabled", value = "false")})
+        @WebInitParam(name = "vaadin.productionMode", value = "false"),
+        @WebInitParam(name = "devmode.gizmo.enabled", value = "true") })
 public class TestServlet extends QuarkusVaadinServlet {
     // This servlet is here to give the suppressJSR356 parameter and disable
     // dev mode gizmo which triggers unsupported web-sockets connection
