aa4d74106d4690644a5fa9cd33b2fad9fc94a7e1
Marco Collovati <marco@vaadin.com>
2022-12-07 09:14:36 +0100

    fix: make sure annotated endpoints works along Vaadin PUSH (#90)
    The workaround added to make PUSH work in Quarkus prevents the usage of custom
    annotated websocket endpoint. This change allow them to work along Vaadin PUSH.
    
    Fixes #78

---
+++ integration-tests/custom-websocket-dependency/src/main/java/org/vaadin/sample/websockets/DependencyAnnotatedWS.java
@@ -0,0 +1,23 @@
+package org.vaadin.sample.websockets;
+
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+@ServerEndpoint(DependencyAnnotatedWS.URI)
+public class DependencyAnnotatedWS {
+
+    public static final String URI = "/dependency-annotated-websocket";
+    public static final String PREFIX = ">> Dependency Annotated Endpoint: ";
+
+    @OnOpen
+    public void onOpen(Session session) {
+        session.getAsyncRemote().sendText(PREFIX + "Welcome");
+    }
+
+    @OnMessage
+    public void onMessage(String message, Session session) {
+        session.getAsyncRemote().sendText(PREFIX + message);
+    }
+}
