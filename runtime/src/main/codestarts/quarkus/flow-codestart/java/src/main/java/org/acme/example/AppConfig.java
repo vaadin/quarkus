ca13046245aa79a4b48714fad641aca96d1b5ef4
Marco Collovati <marco@vaadin.com>
2023-05-10 15:45:35 +0200

    feat: added codestart (#119)
    Added a codestart that provides a Flow application with an example view
    
    Closes #112
    Co-authored-by: Peter Czuczor <61667986+czp13@users.noreply.github.com>

---
+++ runtime/src/main/codestarts/quarkus/flow-codestart/java/src/main/java/org/acme/example/AppConfig.java
@@ -0,0 +1,8 @@
+package org.acme.example;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
+
+@Theme("starter-theme")
+public class AppConfig implements AppShellConfigurator {
+}
