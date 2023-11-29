152d80035418dc377b9e2ecf781fea6f346335e9
Marco Collovati <marco@vaadin.com>
2023-11-29 18:17:46 +0100

    test: allow remote connection to debug window
    Prevents error logs on browser console that make some test fail

--- integration-tests/common-test-code/src/test/java/com/vaadin/flow/test/AbstractChromeIT.java
+++ integration-tests/common-test-code/src/test/java/com/vaadin/flow/test/AbstractChromeIT.java
@@ -3,6 +3,12 @@ package com.vaadin.flow.test;
 import java.util.Arrays;
 import java.util.List;
 
+import com.vaadin.flow.testcategory.ChromeTests;
+import com.vaadin.flow.testutil.ChromeBrowserTest;
+import com.vaadin.testbench.annotations.BrowserConfiguration;
+import com.vaadin.testbench.parallel.Browser;
+import com.vaadin.testbench.parallel.BrowserUtil;
+import io.quarkus.test.common.QuarkusTestResource;
 import org.junit.experimental.categories.Category;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -11,19 +17,14 @@ import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-import com.vaadin.flow.testcategory.ChromeTests;
-import com.vaadin.flow.testutil.ChromeBrowserTest;
-import com.vaadin.testbench.annotations.BrowserConfiguration;
-import com.vaadin.testbench.parallel.Browser;
-import com.vaadin.testbench.parallel.BrowserUtil;
-
 /**
  * Simplified chrome test that doesn't handle view/IT class paths. Uses Jupiter
  * API
  */
 @Category(ChromeTests.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@ExtendWith(ScreenshotsOnFailureExtension.class)
+@ExtendWith({ ScreenshotsOnFailureExtension.class })
+@QuarkusTestResource(VaadinSystemPropertiesPropagator.class)
 public abstract class AbstractChromeIT extends ChromeBrowserTest {
 
     @AfterEach
