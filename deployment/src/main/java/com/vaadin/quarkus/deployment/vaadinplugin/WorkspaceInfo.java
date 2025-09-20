e05f0020d0718ab678d689722aeed65beb764882
Marco Collovati <marco@vaadin.com>
2025-09-20 14:54:05 +0200

    feat: add embedded Vaadin plugin
    Add comprehensive Vaadin build tooling integration that embeds the Vaadin
    frontend build process directly into the Quarkus build lifecycle, eliminating
    the need for external Maveni/Gradle plugin configuration.
    
    Configuration is available through `vaadin.build.*` properties with
    comprehensive options for customizing the build process.
    
    It also defines vaadin-dev as a development dependency so that it
    will be automatically added when the application is started in
    dev mode.
    
    The result of the changes is that an application using Vaadin Quarkus
    extension does not need a specific production profile to generate
    a production build.
    
    CAVEATS:
    - the vaadin-dev dependency version must be hard-coded in the extension
    runtime POM file, so if a different version is required for the
    application, an explicit dependency must be added to the build tool
    configuration.
    - the definition of the automatic vaadin-dev should be done in the
    platform extension
    - The plugin requires information about the project to correctly build
    fronted resources; unfortunately the information are not available
    at build time unless the quarkus.bootstrap.workspace-discovery flag
    is activate. To overcome to this limitation, the extension provides
    a CodeGenProvider that collects the required info when executed by
    quarkus generate-code and generate-code-tests goals and writes them
    into the build folder to be able to read them again in the build steps.

---
+++ deployment/src/main/java/com/vaadin/quarkus/deployment/vaadinplugin/WorkspaceInfo.java
@@ -0,0 +1,115 @@
+package com.vaadin.quarkus.deployment.vaadinplugin;
+
+import com.vaadin.flow.internal.JacksonUtils;
+import io.quarkus.bootstrap.workspace.*;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * The WorkspaceInfo class provides methods for saving and loading project
+ * workspace information related to a quarkus application. This information
+ * includes details about the module's directory, build directory, source
+ * directories, and resource directories. The class uses JSON-based
+ * serialization and deserialization for the persistence of workspace
+ * information.
+ */
+class WorkspaceInfo {
+
+    private record ProjectInfo(String groupId, String artifactId,
+            String version, String moduleDir, String buildDir,
+            List<SourceDirInfo> sourceDirs, List<SourceDirInfo> resourceDirs) {
+
+        WorkspaceModuleId moduleId() {
+            return WorkspaceModuleId.of(groupId, artifactId, version);
+        }
+    }
+
+    private record SourceDirInfo(String dir, String outputDir) {
+        SourceDirInfo(SourceDir sourceDir) {
+            this(sourceDir.getDir().toFile().getAbsolutePath(),
+                    sourceDir.getOutputDir().toFile().getAbsolutePath());
+        }
+    }
+
+    /**
+     * Saves workspace information of the given module to the specified working
+     * directory.
+     *
+     * @param module
+     *            the workspace module containing the information to be saved
+     * @param workDir
+     *            the directory where the workspace information will be written
+     *            to
+     * @throws Exception
+     *             if an error occurs while writing the workspace information
+     */
+    static void save(WorkspaceModule module, Path workDir) throws Exception {
+        Path projectInfoFile = resolveProjectInfoFile(workDir);
+        var info = collectWorkspaceInfo(module);
+        Files.writeString(projectInfoFile,
+                JacksonUtils.getMapper().writeValueAsString(info),
+                StandardOpenOption.CREATE);
+    }
+
+    /**
+     * Loads a {@code WorkspaceModule} instance from the specified working
+     * directory. If the project information file exists, it reads the project
+     * details and constructs a {@code WorkspaceModule} object. If the file does
+     * not exist, it returns {@code null}.
+     *
+     * @param workDir
+     *            the directory where the project information file is located
+     * @return a {@code WorkspaceModule} object built from the project
+     *         information file, or {@code null} if the file does not exist
+     * @throws Exception
+     *             if an error occurs while reading or parsing the project
+     *             information file
+     */
+    static WorkspaceModule load(Path workDir) throws Exception {
+        Path projectInfoFile = resolveProjectInfoFile(workDir);
+        if (Files.exists(projectInfoFile)) {
+            var info = JacksonUtils.getMapper()
+                    .readValue(projectInfoFile.toFile(), ProjectInfo.class);
+            return WorkspaceModule.builder().setModuleId(info.moduleId())
+                    .setModuleDir(Path.of(info.moduleDir()))
+                    .setBuildDir(Path.of(info.buildDir()))
+                    .addArtifactSources(new DefaultArtifactSources(
+                            ArtifactSources.MAIN,
+                            info.sourceDirs.stream()
+                                    .map(d -> SourceDir.of(Path.of(d.dir()),
+                                            Path.of(d.outputDir())))
+                                    .toList(),
+                            info.resourceDirs.stream()
+                                    .map(d -> SourceDir.of(Path.of(d.dir()),
+                                            Path.of(d.outputDir())))
+                                    .toList()))
+                    .build();
+        }
+        return null;
+    }
+
+    private static Path resolveProjectInfoFile(Path workDir) {
+        return workDir.resolve("vaadin-plugin-project-info.txt");
+    }
+
+    private static ProjectInfo collectWorkspaceInfo(WorkspaceModule module) {
+        List<SourceDirInfo> sourceDirs = null;
+        List<SourceDirInfo> resourceDirs = null;
+        if (module.hasMainSources()) {
+            sourceDirs = module.getMainSources().getSourceDirs().stream()
+                    .map(SourceDirInfo::new).collect(Collectors.toList());
+            resourceDirs = module.getMainSources().getResourceDirs().stream()
+                    .map(SourceDirInfo::new).collect(Collectors.toList());
+        }
+        WorkspaceModuleId moduleId = module.getId();
+        return new ProjectInfo(moduleId.getGroupId(), moduleId.getArtifactId(),
+                moduleId.getVersion(), module.getModuleDir().getAbsolutePath(),
+                module.getBuildDir().getAbsolutePath(), sourceDirs,
+                resourceDirs);
+    }
+
+}
