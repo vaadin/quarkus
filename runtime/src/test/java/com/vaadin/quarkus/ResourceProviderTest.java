9879be3e4843eac3b83528d20cf434df0e839927
Marco Collovati <marco@vaadin.com>
2025-11-06 07:16:42 +0100

    test: replace elemental with Jackson in test

--- runtime/src/test/java/com/vaadin/quarkus/ResourceProviderTest.java
+++ runtime/src/test/java/com/vaadin/quarkus/ResourceProviderTest.java
@@ -7,12 +7,11 @@ import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.vaadin.flow.internal.JacksonUtils;
 import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import elemental.json.Json;
-import elemental.json.JsonObject;
+import tools.jackson.databind.node.ObjectNode;
 
 public class ResourceProviderTest {
 
@@ -50,7 +49,7 @@ public class ResourceProviderTest {
 
         String content = IOUtils.readLines(stream, StandardCharsets.UTF_8)
                 .stream().collect(Collectors.joining("\n"));
-        JsonObject object = Json.parse(content);
-        Assertions.assertTrue(object.getBoolean("client-resource"));
+        ObjectNode object = JacksonUtils.readTree(content);
+        Assertions.assertTrue(object.get("client-resource").asBoolean());
     }
 }
