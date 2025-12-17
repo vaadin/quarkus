aa242e100ca1206a087a5add39d187f1d39e3bc5
Marco Collovati <marco@vaadin.com>
2025-12-17 07:34:57 +0100

    chore: add native build validation (#247)

--- integration-tests/common-test-code/src/main/java/com/vaadin/flow/quarkus/it/AppShellConfig.java
+++ integration-tests/common-test-code/src/main/java/com/vaadin/flow/quarkus/it/AppShellConfig.java
@@ -3,8 +3,10 @@ package com.vaadin.flow.quarkus.it;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.theme.Theme;
+import io.quarkus.runtime.annotations.RegisterForReflection;
 
 @Theme("reusable-theme")
 @Push
+@RegisterForReflection(classNames = "org.vaadin.sample.websockets.SimpleEndpoint")
 public class AppShellConfig implements AppShellConfigurator {
 }
